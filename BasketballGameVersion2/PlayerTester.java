9233a59779db515f89e7484ca3b46e195e64b397
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-08-28 23:40:39 +0800

    random

--- BasketballGameVersion2/PlayerTester.java
+++ BasketballGameVersion2/PlayerTester.java
@@ -3,13 +3,8 @@ public class PlayerTester {
         PlayerV2 p1 = new PlayerV2("James");
         PlayerV2 p2 = new PlayerV2("Bryant");
 
-        BlacktopOnes game1 = new BlacktopOnes(21);
-        p1.setHasBall(true);
-        System.out.println(p1.getPlayerName() + " : " + p1.getHasBall());
-        System.out.println(p2.getPlayerName() + " : " + p2.getHasBall());
-        p2.reboundBall(p1);
-        p2.showActions();
-        System.out.println(p1.getPlayerName() + " : " + p1.getHasBall());
-        System.out.println(p2.getPlayerName() + " : " + p2.getHasBall());
+        BlacktopOnes game1 = new BlacktopOnes();
+        game1.startNewGame(p1, p2);
+        game1.showHightlights();
     }    
 }
