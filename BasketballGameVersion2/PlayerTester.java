30b0fa01688c0c1fa9279ef97683627faac90098
Leander Lubguban <Lorenzolubguban@gmail.com>
2023-08-28 23:39:05 +0800

    update for blacktop

--- BasketballGameVersion2/PlayerTester.java
+++ BasketballGameVersion2/PlayerTester.java
@@ -2,14 +2,19 @@ public class PlayerTester {
     public static void main(String[] args){
         PlayerV2 p1 = new PlayerV2("James");
         PlayerV2 p2 = new PlayerV2("Bryant");
+        PlayerV2 p3 = new PlayerV2("Curry");
+        PlayerV2 p4 = new PlayerV2("Harden");
 
-        BlacktopOnes game1 = new BlacktopOnes(21);
-        p1.setHasBall(true);
-        System.out.println(p1.getPlayerName() + " : " + p1.getHasBall());
-        System.out.println(p2.getPlayerName() + " : " + p2.getHasBall());
-        p2.reboundBall(p1);
-        p2.showActions();
-        System.out.println(p1.getPlayerName() + " : " + p1.getHasBall());
-        System.out.println(p2.getPlayerName() + " : " + p2.getHasBall());
+        // BlacktopOnes game1 = new BlacktopOnes();
+        // game1.startNewGame(p1, p2);
+        // game1.showHightlights();
+
+        TournamentSingle t1 = new TournamentSingle();
+        t1.add(p1);
+        t1.add(p2);
+        t1.add(p3);
+        t1.add(p4);
+        t1.startTournament();
+        t1.showTournamentHighlights();
     }    
 }
--- Chapter_94_RecursionString/RecursionStringTester.java
+++ Chapter_94_RecursionString/RecursionStringTester.java
@@ -3,13 +3,12 @@ public class RecursionStringTester {
         // CountChar count = new CountChar();
         // VowelRemover rem = new VowelRemover();
         // MatchString match = new MatchString();
-        MatchCount mat = new MatchCount();
+        // MatchCount mat = new MatchCount();
+        Palindrome pal = new Palindrome();
         // String word = "AUDIOBOOK";
         // char c = 'e';
         // System.out.println("The letter " + c + " in the word " + word + " appears : " + count.countChar(word, c) + " times");
-        String word1 = "XXXX";
-        String word2 = "XX";
-        // System.out.println(match.matchWith(word1, word2));;
-        System.out.println(mat.matchCount(word1, word2));
+        String word1 = "Ah! Satan sees Natasha.";
+        System.out.println("Palindrome = " + pal.isPalindrome(word1));
     }
 }
